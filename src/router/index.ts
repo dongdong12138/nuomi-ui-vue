7127205880815c148c3d7eedb1de5a79e8f92004
BarryDong <286084845@qq.com>
2023-12-10 13:46:00 +0800

    创建文档页

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue';
+import Doc from '@/views/Doc.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
+    { path: '/', name: 'home', component: Home },
+    { path: '/doc', name: 'doc', component: Doc },
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/Doc.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TopNav from '@/components/TopNav.vue';
+</script>
+
+<template>
+  <top-nav />
+</template>
+
+<style scoped lang="scss">
+
+</style>
