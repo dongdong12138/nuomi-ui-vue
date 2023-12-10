2367b1812b44020905695cbdf3b4a59ae95a3520
BarryDong <286084845@qq.com>
2023-12-10 13:04:20 +0800

    封装 TopNav.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,11 @@
 <script setup lang="ts">
+import TopNav from '@/components/TopNav.vue';
 </script>
 
 <template>
-  <div class="topNav">
-
-    <div class="logo">
-      <router-link to="/">
-        <svg class="icon">
-          <use xlink:href="#icon-nuomi"></use>
-        </svg>
-      </router-link>
-    </div>
-
-    <ul class="menu">
-      <li>
-        <a href="https://github.com/dongdong12138/nuomi-ui-vue.git" target="_blank">
-          <svg class="icon">
-            <use xlink:href="#icon-github"></use>
-          </svg>
-        </a>
-      </li>
-      <li>
-        <a href="https://gitee.com/barrydong/nuomi-ui-vue.git" target="_blank">
-          <svg class="icon">
-            <use xlink:href="#icon-gitee"></use>
-          </svg>
-        </a>
-      </li>
-    </ul>
-
-  </div>
+  <top-nav />
 </template>
 
 <style scoped lang="scss">
-.topNav {
-  box-shadow: 0 5px 5px rgba(51,51,51,.1);
-  display: flex; justify-content: center; align-items: center;
-  padding: 16px;
-}
 
-.logo {
-  margin-right: auto;
-  > a {
-    display: inline-block;
-    > svg { font-size: 3em; }
-  }
-}
-
-.menu {
-  display: flex;
-  > li {
-    &:last-child { margin-left: 10px; }
-    > a {
-      display: inline-block;
-      width: 24px; height: 24px;
-      > svg {
-        width: 100%; height: 100%;
-        vertical-align: middle;
-      }
-    }
-  }
-}
 </style>
