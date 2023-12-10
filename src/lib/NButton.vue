77d29c2f6f8a9a5d4b64c4ba860ee6eb8a6b04dd
BarryDong <286084845@qq.com>
2023-12-10 15:49:46 +0800

    Button 组件基础样式

---
+++ src/lib/NButton.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="nuomi-button">按钮</button>
+</template>
+
+<style scoped lang="scss">
+$color: #333;
+$h: 32px;
+$border-color: #d9d9d9;
+$radius: 4px;
+$blue: #40a9ff;
+
+// 基础样式
+.nuomi-button {
+  box-sizing: border-box;
+  color: $color;
+  height: $h;
+  background: #fff;
+  border: 1px solid $border-color; border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(#000, 0.95);
+  cursor: pointer; white-space: nowrap;
+  display: inline-flex; justify-content: center; align-items: center;
+  padding: 0 12px;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover, &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
+</style>
--- src/views/ButtonDoc.vue
+++ src/views/ButtonDoc.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-
+import NButton from '@/lib/NButton.vue';
 </script>
 
 <template>
-  <div>Button 组件</div>
+  <h3>Button 组件示例</h3>
+  <n-button />
+  <n-button />
+  <n-button />
 </template>
 
 <style scoped lang="scss">
