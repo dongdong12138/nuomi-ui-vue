ea3e74b531a07ab9b2a6e91e3194aff650ba70c3
BarryDong <286084845@qq.com>
2023-12-10 22:00:21 +0800

    Updates

--- assets/Intro-qow4humm.js
+++ assets/Intro-qow4humm.js
@@ -1,7 +1,7 @@
-import{d as e,h as o,o as t,c as r,u as c}from"./index-FNBE2Nhf.js";const n=e({name:"Markdown"}),i=()=>o("div",{innerHTML:`<h1>介绍</h1>
+import{d as e,h as o,o as t,c as r,u as c}from"./index-4V1HO5H9.js";const n=e({name:"Markdown"}),i=()=>o("div",{innerHTML:`<h1>介绍</h1>
 <p>糯米 UI 是一款基于 Vue3 和 TypeScript 的 UI 组件库，提供了 <code>Button</code>、<code>Switch</code>、<code>Tabs</code>、<code>Dialog</code> 等组件。</p>
 <p>每个组件的源代码都简洁易懂，项目的 commit 也很清晰，欢迎各位同学进行阅读和讨论，源码地址：<a href="https://github.com/dongdong12138/nuomi-ui-vue.git">Github</a> | <a href="https://gitee.com/barrydong/nuomi-ui-vue.git">Gitee</a>。</p>
 <p><strong>注意，此项目仅用于学习用途，不建议用于生产环境！</strong></p>
 <p>本站采用的技术栈：Vue3、Vite、TypeScript、VueRouter、pnpm 等。</p>
-<p>下一节：<a href="/doc/install">安装</a></p>
+<p>下一节：<a href="#/doc/install">安装</a></p>
 `});n.render=i;const p=["innerHTML"],m=e({__name:"Intro",setup(a){return(d,s)=>(t(),r("article",{innerHTML:c(n).render().props.innerHTML,class:"markdown-body"},null,8,p))}});export{m as default};
---
+++ assets/SwitchDoc-AA32WMqA.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as t}from"./index-4V1HO5H9.js";const o={};function n(r,s){return e(),t("div",null,"Switch 组件")}const a=c(o,[["render",n]]);export{a as default};
