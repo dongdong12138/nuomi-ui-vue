4e842552b4272aea9af6cbaab104dc00a5047d4c
BarryDong <286084845@qq.com>
2023-12-12 21:55:55 +0800

    导出 openDialog 方法

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as NButton } from './NButton.vue';
 export { default as NSwitch } from './NSwitch.vue';
 export { default as NDialog } from './NDialog.vue';
+export { openDialog } from './openDialog'
