36af23ceda3e99dfac0f988b6b019ffa07dd8ab7
BarryDong <286084845@qq.com>
2023-12-13 14:39:59 +0800

    Updates

--- assets/GetStarted-WA9sToyb.js
+++ assets/GetStarted-WA9sToyb.js
@@ -1,4 +1,4 @@
-import{d as n,h as e,c as o,u as r,o as s}from"./index-pcgP_JKC.js";const t=n({name:"Markdown"}),a=()=>e("div",{innerHTML:`<h1>开始使用</h1>
+import{d as n,h as e,c as o,u as r,o as s}from"./index-ee7QsQGF.js";const t=n({name:"Markdown"}),a=()=>e("div",{innerHTML:`<h1>开始使用</h1>
 <p>请先<a href="#/doc/install">安装</a>本组件库。</p>
 <p>在 main.ts 中引入组件库样式：</p>
 <pre><code class="language-ts">import &#39;nuomi-ui-vue/dist/lib/nuomi.css&#39;;
