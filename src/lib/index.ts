9c87e46140cffab7b726b18325cdf1090b04e277
BarryDong <286084845@qq.com>
2023-12-13 14:54:41 +0800

    导出 Tabs、Tab 组件 & 发布新版本到 npm

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 export { default as NButton } from './NButton.vue';
 export { default as NSwitch } from './NSwitch.vue';
 export { default as NDialog } from './NDialog.vue';
-export { openDialog } from './openDialog'
+export { openDialog } from './openDialog';
+export { default as NTabs } from './NTabs.vue';
+export { default as NTab } from './NTab.vue';
