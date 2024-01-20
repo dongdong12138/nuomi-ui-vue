5d4a89bf602f6f6081c74424e6bddf327228df2e
BarryDong <286084845@qq.com>
2024-01-20 12:52:40 +0800

    Container 组件 - 综合布局2

---
+++ src/views/ContainerDoc/ContainerDemo4.vue
@@ -0,0 +1,20 @@
+<demo>综合布局2</demo>
+
+<script setup lang="ts">
+import NContainer from '@/lib/Container/NContainer.vue';
+import NHeader from '@/lib/Container/NHeader.vue';
+import NMain from '@/lib/Container/NMain.vue';
+import NFooter from '@/lib/Container/NFooter.vue';
+import NAside from '@/lib/Container/NAside.vue';
+</script>
+
+<template>
+  <n-container>
+    <n-aside>Aside</n-aside>
+    <n-container>
+      <n-header>Header</n-header>
+      <n-main>Main</n-main>
+      <n-footer>Footer</n-footer>
+    </n-container>
+  </n-container>
+</template>
--- src/views/ContainerDoc/index.vue
+++ src/views/ContainerDoc/index.vue
@@ -3,6 +3,7 @@ import VueDemo from '@/components/VueDemo.vue';
 import ContainerDemo1 from './ContainerDemo1.vue';
 import ContainerDemo2 from './ContainerDemo2.vue';
 import ContainerDemo3 from './ContainerDemo3.vue';
+import ContainerDemo4 from './ContainerDemo4.vue';
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import ContainerDemo3 from './ContainerDemo3.vue';
   <vue-demo :component="ContainerDemo1" />
   <vue-demo :component="ContainerDemo2" />
   <vue-demo :component="ContainerDemo3" />
+  <vue-demo :component="ContainerDemo4" />
 </template>
