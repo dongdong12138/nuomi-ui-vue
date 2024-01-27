fb0dcc8b2a088f16adfc30d0ef57d8969a1d7498
BarryDong <286084845@qq.com>
2024-01-27 21:42:40 +0800

    导出 Input 相关组件

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,6 @@ export { default as NDialog } from './NDialog.vue';
 export { openDialog } from './openDialog';
 export { default as NTabs } from './NTabs.vue';
 export { default as NTab } from './NTab.vue';
+export { default as NInput } from './Input/NInput.vue';
+export { default as NInputPassword } from './Input/NInputPassword.vue';
+export { default as NInputArea } from './Input/NInputArea.vue';
