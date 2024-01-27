3b80be4208e7abccff1bd4a0cc92ff15da1cfa4d
BarryDong <286084845@qq.com>
2024-01-27 19:52:57 +0800

    Input 组件 - 密码框逻辑

--- src/lib/Input/NInputPassword.vue
+++ src/lib/Input/NInputPassword.vue
@@ -1,26 +1,63 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+withDefaults(defineProps<{
+  modelValue: string
+  placeholder?: string
+}>(), {
+  placeholder: '请输入'
+});
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>();
+
+const pwdVisible = ref(false);
+const inputType = ref('password');
+
+const onInput = (event) => {
+  emit('update:modelValue', event.target.value);
+};
+const changeVisible = () => {
+  pwdVisible.value = !pwdVisible.value;
+  inputType.value = inputType.value === 'password' ? 'text' : 'password';
+};
 </script>
 
 <template>
   <div class="nuomi-input-wrapper">
-    <input type="password">
+    <input :type="inputType" :value="modelValue" :placeholder="placeholder" @input="onInput">
+    <span class="svgContainer" @click="changeVisible">
+      <svg class="icon">
+        <use :xlink:href="pwdVisible ? '#icon-eye' : '#icon-eye-close'"></use>
+      </svg>
+    </span>
   </div>
 </template>
 
 <style lang="scss">
+$hoverColor: #40a9ff;
+
 .nuomi-input-wrapper {
   color: #000000d9; font-size: 14px;
+  width: 100%;
   background-color: #fff;
   border: 1px solid #d9d9d9; border-radius: 2px;
+  display: inline-flex; align-items: center;
   padding: 0 11px;
   &:hover {
-    border-color: #40a9ff;
-    border-right-width: 1px !important;
+    border-color: $hoverColor;
+  }
+  &:focus-within {
+    outline: $hoverColor;
+    border-color: $hoverColor;
   }
   > input {
     width: 100%; height: 30px;
     border: none; outline: none;
+  }
+  .svgContainer {
+    cursor: pointer;
   }
 }
 </style>
--- src/views/InputDoc/InputDemo4.vue
+++ src/views/InputDoc/InputDemo4.vue
@@ -1,9 +1,14 @@
 <demo>密码框</demo>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import NInputPassword from '@/lib/Input/NInputPassword.vue';
+
+const inputValue = ref('');
+
 </script>
 
 <template>
-  <n-input-password></n-input-password>
+  <n-input-password v-model="inputValue" placeholder="请输入密码" />
+  <p>{{ inputValue }}</p>
 </template>
