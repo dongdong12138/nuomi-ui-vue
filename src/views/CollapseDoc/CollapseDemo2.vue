1d28f72db5bcf7258ad8af3e8b976a48875461d1
BarryDong <286084845@qq.com>
2024-01-28 22:00:01 +0800

    Collapse 组件文档

---
+++ src/views/CollapseDoc/CollapseDemo2.vue
@@ -0,0 +1,17 @@
+<demo>支持一次展开一个</demo>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import NCollapse from '@/lib/Collapse/NCollapse.vue';
+import NCollapseItem from '@/lib/Collapse/NCollapseItem.vue';
+
+const activeKey = ref([1]);
+</script>
+
+<template>
+  <n-collapse v-model:active-key="activeKey" accordion>
+    <n-collapse-item :item-key="1" header="标题1">内容1</n-collapse-item>
+    <n-collapse-item :item-key="2" header="标题2">内容2</n-collapse-item>
+    <n-collapse-item :item-key="3" header="标题3">内容3</n-collapse-item>
+  </n-collapse>
+</template>
--- src/views/CollapseDoc/index.vue
+++ src/views/CollapseDoc/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import VueDemo from '@/components/VueDemo.vue';
 import CollapseDemo1 from './CollapseDemo1.vue';
+import CollapseDemo2 from './CollapseDemo2.vue';
 </script>
 
 <template>
   <h2>Collapse 组件示例</h2>
   <vue-demo :component="CollapseDemo1" />
+  <vue-demo :component="CollapseDemo2" />
 </template>
